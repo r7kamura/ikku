513fde9119795fd34ae76a671a3dd20724dad407
Ryo Nakamura <r7kamura@gmail.com>
2015-02-20 02:16:27 +0900

    Fix bracket bug

--- lib/ikku/bracket_state.rb
+++ lib/ikku/bracket_state.rb
@@ -1,23 +1,35 @@
 module Ikku
   class BracketState
-    TABLE = {
-      "’" => "‘",
-      "”" => "“",
-      "）" => "（",
-      ")" => "(",
-      "］" => "［",
-      "]" => "[",
-      "}" => "{",
-      "｝" => "｛",
-      "〉" => "〈",
-      "》" => "《",
-      "」" => "「",
-      "』" => "『",
-      "】" => "【",
-      "〕" => "〔",
-      ">" => "<",
-      "＞" => "＜",
+    BRACKETS_TABLE = {
+      "‘" => "’",
+      "“" => "”",
+      "（" => "）",
+      "(" => ")",
+      "［" => "］",
+      "[" => "]",
+      "{" => "}",
+      "｛" => "｝",
+      "〈" => "〉",
+      "《" => "》",
+      "「" => "」",
+      "『" => "』",
+      "【" => "】",
+      "〔" => "〕",
+      "<" => ">",
+      "＜" => "＞",
     }
+
+    class << self
+      def brackets_index
+        @brackets_index ||= BRACKETS_TABLE.to_a.flatten.inject({}) do |hash, bracket|
+          hash.merge(bracket => true)
+        end
+      end
+
+      def inverted_brackets_table
+        @inverted_brackets_table ||= BRACKETS_TABLE.invert
+      end
+    end
 
     def consume_all(surfaces)
       surfaces.each do |surface|
@@ -34,10 +46,10 @@ module Ikku
 
     def consume(surface)
       case
-      when TABLE.values.include?(surface)
-        stack.push(surface)
-      when TABLE.include?(surface) && stack.last == TABLE[surface]
+      when !stack.last.nil? && self.class.inverted_brackets_table[surface] == stack.last
         stack.pop
+      when self.class.brackets_index.include?(surface)
+        stack.push(surface)
       end
     end
 
